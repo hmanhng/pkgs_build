4f003b49c90e8e47110eb2ebc7c29302cc91452f
hmanhng <hmanhng@icloud.com>
2022-10-26 22:07:12 +0700

    kde skel

---
+++ hmanhng-skeleton-kde/files/qutebrowser/config.py
@@ -0,0 +1,94 @@
+# vim:fileencoding=utf-8:foldmethod=marker
+from qutebrowser.config.configfiles import ConfigAPI  # noqa: F401,E501 pylint: disable=unused-import
+from qutebrowser.config.config import ConfigContainer  # noqa: F401,E501 pylint: disable=unused-import
+config = config  # type: ConfigAPI # noqa: F821 pylint: disable=E0602,C0103
+c = c  # type: ConfigContainer # noqa: F821 pylint: disable=E0602,C0103
+config.load_autoconfig()
+c.aliases = {'q': 'quit', 'w': 'session-save', 'wq': 'quit --save'}
+# ============================== AdBlocks ============================== {{{
+c.content.blocking.adblock.lists = [
+        "https://easylist.to/easylist/easylist.txt",
+        "https://easylist.to/easylist/easyprivacy.txt",
+        "https://easylist.to/easylist/fanboy-social.txt",
+        "https://secure.fanboy.co.nz/fanboy-annoyance.txt",
+        "https://easylist-downloads.adblockplus.org/abp-filters-anti-cv.txt",
+        #"https://gitlab.com/curben/urlhaus-filter/-/raw/master/urlhaus-filter.txt",
+        "https://pgl.yoyo.org/adservers/serverlist.php?showintro=0;hostformat=hosts",
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances.txt",
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badlists.txt",
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badware.txt",
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters.txt",
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/legacy.txt",
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/privacy.txt",
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/resource-abuse.txt",
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/unbreak.txt",
+        "https://www.i-dont-care-about-cookies.eu/abp/",
+        "https://secure.fanboy.co.nz/fanboy-cookiemonster.txt"]
+c.content.blocking.hosts.lists = ["https://raw.githubusercontent.com/bigdargon/hostsVN/master/hosts"]
+c.content.blocking.method = 'both'
+# }}}
+c.downloads.location.directory = '~/Downloads'
+c.editor.command = ['kitty', 'lvim', '{file}']
+c.statusbar.show = 'always'
+c.tabs.show = 'always'
+c.url.searchengines = {'DEFAULT': 'https://www.google.com/search?q={}', 'aw': 'https://wiki.archlinux.org/?search={}', 're': 'https://www.reddit.com/r/{}', 'ub': 'https://www.urbandictionary.com/define.php?term={}', 'wiki': 'https://en.wikipedia.org/wiki/{}', 'yt': 'https://www.youtube.com/results?search_query={}'}
+c.url.default_page = 'https://github.com/hmanhng/'
+c.url.start_pages = 'qute://bindings'
+# c.zoom.default = '120'
+# ============================== Colors ============================== {{{
+c.colors.completion.fg = ['#9cc4ff', 'white', 'white']
+c.colors.completion.odd.bg = '#1c1f24'
+c.colors.completion.even.bg = '#232429'
+c.colors.completion.category.fg = '#e1acff'
+c.colors.completion.category.bg = 'qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0 #000000, stop:1 #232429)'
+c.colors.completion.category.border.top = '#3f4147'
+c.colors.completion.category.border.bottom = '#3f4147'
+c.colors.completion.item.selected.fg = '#282c34'
+c.colors.completion.item.selected.bg = '#ecbe7b'
+c.colors.completion.item.selected.match.fg = '#c678dd'
+c.colors.completion.match.fg = '#c678dd'
+c.colors.completion.scrollbar.fg = 'white'
+c.colors.downloads.bar.bg = '#282c34'
+c.colors.downloads.error.bg = '#ff6c6b'
+c.colors.hints.fg = '#282c34'
+c.colors.hints.match.fg = '#98be65'
+c.colors.messages.info.bg = '#282c34'
+c.colors.statusbar.normal.bg = '#282c34'
+c.colors.statusbar.insert.fg = 'white'
+c.colors.statusbar.insert.bg = '#497920'
+c.colors.statusbar.passthrough.bg = '#34426f'
+c.colors.statusbar.command.bg = '#282c34'
+c.colors.statusbar.url.warn.fg = 'yellow'
+c.colors.tabs.bar.bg = '#1c1f34'
+c.colors.tabs.odd.bg = '#282c34'
+c.colors.tabs.even.bg = '#282c34'
+c.colors.tabs.selected.odd.bg = '#b46fcc'
+c.colors.tabs.selected.even.bg = '#b46fcc'
+c.colors.tabs.pinned.odd.bg = 'seagreen'
+c.colors.tabs.pinned.even.bg = 'darkseagreen'
+c.colors.tabs.pinned.selected.odd.bg = '#282c34'
+c.colors.tabs.pinned.selected.even.bg = '#282c34'
+# }}}
+# ============================== Fonts ============================== {{{
+c.fonts.default_family = '"Iosevka"'
+c.fonts.default_size = '12pt'
+c.fonts.completion.entry = 'default_size default_family'
+c.fonts.debug_console = 'default_size default_family'
+c.fonts.prompts = 'default_size sans-serif'
+c.fonts.statusbar = 'default_size default_family'
+# }}}
+# ============================== Bind ============================== {{{
+config.bind(',d', 'hint links spawn kitty yt-dlp {hint-url}')
+config.bind(',m', 'hint links spawn mpv --profile=M60 {hint-url}')
+config.bind('t', 'set-cmd-text -s :open -t')
+config.bind('wr', 'config-source')
+config.bind('ww', 'config-write-py -f')
+config.bind('xb', 'config-cycle statusbar.show always never')
+config.bind('xt', 'config-cycle tabs.show always never')
+config.bind('xx', 'config-cycle statusbar.show always never;; config-cycle tabs.show always never')
+config.bind(';t', 'hint userscript link translate')
+config.bind(';T', 'hint userscript all translate --text')
+config.bind(',t', 'spawn --userscript translate')
+config.bind(',T', 'spawn --userscript translate --text')
+config.bind('<Ctrl+r>', 'restart')
+# }}}
